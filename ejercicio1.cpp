23e91baa66342409c79956889b8555bd86d0c073
Shaiel <st305773@fi365.ort.edu.uy>
2026-03-26 16:18:36 -0300

    ej1

--- ejercicio1.cpp
+++ ejercicio1.cpp
@@ -7,5 +7,31 @@ using namespace std;
 
 int main()
 {
+    int n;
+    cin >> n;
+    tabla t = crearTabla(n);
+    int pedir = 0;
+    string datos;
+    cin >> datos >> endln;
+    for (int i = 0; i < datos.length(); i++)
+    {
+        if (i % 2 == 0)
+        {
+            insertar(t, datos[i]);  
+        }
+        else
+        {
+            char puerta = datos[i] - 'A' + 'a';  
+            if (buscar(t, puerta) == 0)
+            {
+                pedir++;
+            }
+            else
+            {
+                eliminar(t, puerta);
+            }
+        }
+    }
+    cout << pedir << endl;
     return 0;
 }
