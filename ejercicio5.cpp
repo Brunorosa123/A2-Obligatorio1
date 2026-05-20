5774734a4f39fed8a2bed6e05616d972b7f3e56e
Bruno Rosa <bruno.rosa@example.com>
2026-05-20 13:31:40 -0300

    ej5 listo

--- ejercicio5.cpp
+++ ejercicio5.cpp
@@ -2,11 +2,88 @@
 #include <string>
 #include <iostream>
 #include <limits>
+#include "./tads/GrafoLaImp.cpp"
 
 using namespace std;
 
+bool dfs(GrafoLA *grafo, int vertice, int colorAct, int *colores)
+{
+    colores[vertice] = colorAct;
+
+    ListaAdyLA ady = grafo->obtenerAdyacentes(vertice);
+
+    while (ady != NULL)
+    {
+        int dest = ady->vertice;
+
+        if (colores[dest] == -1)
+        {
+            if (!dfs(grafo, dest, 1 - colorAct, colores))
+            {
+                return false;
+            }
+        }
+        else if (colores[dest] == colorAct)
+        {
+            return false;
+        }
+
+        ady = ady->sig;
+    }
+
+    return true;
+}
+
 int main()
 {
-    // TODO
+    int V;
+    int A;
+
+    cin >> V >> A;
+
+    GrafoLA *grafo = new GrafoLA(V, A, false, false);
+
+    for (int i = 0; i < A; i++)
+    {
+        int u;
+        int v;
+
+        cin >> u >> v;
+
+        grafo->insertarArista(u, v, 1);
+    }
+
+    int *colores = new int[V + 1];
+
+    for (int i = 1; i <= V; i++)
+    {
+        colores[i] = -1;
+    }
+
+    bool Bipartito = true;
+
+    for (int i = 1; i <= V; i++)
+    {
+        if (colores[i] == -1)
+        {
+            if (!dfs(grafo, i, 0, colores))
+            {
+                Bipartito = false;
+                break;
+            }
+        }
+    }
+
+    if (Bipartito)
+    {
+        cout << "SI" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    delete[] colores;
+
     return 0;
 }
